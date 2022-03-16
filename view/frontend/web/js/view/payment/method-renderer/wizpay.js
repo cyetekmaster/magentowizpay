c27461aca68a42d951c0ff01f1f6a126ce363d9d
liuyu0723 <liuyu0723@users.noreply.github.com>
2022-03-16 20:48:13 +1300

    new payment 4/7

--- view/frontend/web/js/view/payment/method-renderer/wizpay.js
+++ view/frontend/web/js/view/payment/method-renderer/wizpay.js
@@ -13,10 +13,22 @@ define(
         'Magento_Checkout/js/model/url-builder',
         'mage/url',
         'Magento_Checkout/js/model/quote',
+
+        'Magento_Checkout/js/model/payment/additional-validators',
+        'Magento_Checkout/js/action/set-payment-information',
+        'Magento_Checkout/js/model/error-processor',
+        'Magento_Customer/js/customer-data',
+        'Magento_Customer/js/section-config'
     ],
     function ($, modal, Component, urlBuilder,
         url,
-        quote) {
+        quote,
+        setPaymentInformationAction,
+        additionalValidators,
+        customerData,
+        sectionConfig,
+        errorProcessor
+        ) {
         'use strict';
 
         return Component.extend({
@@ -26,6 +38,44 @@ define(
 
 
             redirectAfterPlaceOrder: false,
+
+
+            continueToWizpay: function(data, event){
+                const self = this;
+
+                if (event) {
+                    event.preventDefault();
+                }
+
+                if (additionalValidators.validate() && this.isPlaceOrderActionAllowed() === true) {
+                    this.isPlaceOrderActionAllowed(false);
+    
+                    setPaymentInformationAction(
+                        self.messageContainer,
+                        self.getData()
+                    ).done(function () {
+    
+                        const captureUrlPath = 'wizpay/index';
+                        console.log('ohoh working now.')
+                        // createAfterpayCheckoutAction(self.messageContainer, {
+                        //     confirmPath: captureUrlPath,
+                        //     cancelPath: captureUrlPath
+                        // }).done(function (response) {
+                        //     const sections = sectionConfig.getAffectedSections(captureUrlPath);
+                        //     customerData.invalidate(sections);
+                        //     $.mage.redirect(response.afterpay_redirect_checkout_url);
+                        // }).always(function () {
+                        //     self.isPlaceOrderActionAllowed(true);
+                        // });
+    
+                    }).fail(function (response) {
+                        errorProcessor.process(response, self.messageContainer);
+                    }).always(function () {
+                        self.isPlaceOrderActionAllowed(true);
+                    });
+                }
+            },
+
 
             getCode: function () {
                 return 'wizpay';
