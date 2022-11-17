eb17eaf0dc6c3b0f3ee51370a93746f0a8a3f683
liuyu0723 <liuyu0723@users.noreply.github.com>
2022-11-17 15:55:43 +1300

    new version 1.1.8

--- Helper/Data.php
+++ Helper/Data.php
@@ -104,7 +104,7 @@ class Data extends AbstractHelper
 
 
     public function getPluginVersion(){
-        return '1.1.7';
+        return '1.1.8';
     }
 
     public function initiateWizpayLogger($log)
