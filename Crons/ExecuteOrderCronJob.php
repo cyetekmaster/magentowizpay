5da20c55a11170f6640630826e433999eaf83e1b
liuyu0723 <liuyu0723@users.noreply.github.com>
2022-11-30 16:37:30 +1300

    update cron: add filter just check two days quotes.

--- Crons/ExecuteOrderCronJob.php
+++ Crons/ExecuteOrderCronJob.php
@@ -36,14 +36,24 @@ class ExecuteOrderCronJob {
         
         $this->logger->info('wizpay - L33 Loading quotes'); 
         $this->quoteCollection = $this->quoteCollectionFactory->create();
-        $this->logger->info('wizpay - L34 Loaded quotes'); 
+        $this->logger->info('wizpay - L34 Loaded quotes');         
+
 
         if($this->quoteCollection && $this->quoteCollection->getSize() > 0){
-            $this->logger->info('wizpay - L38 get total quotes ' . $this->quoteCollection->getSize());
+            $this->logger->info('wizpay - L38 get total quotes before filter ' . $this->quoteCollection->getSize());
+
+            $this->logger->info('wizpay - L45 Add filter for quote'); 
+            $tomorrow = date('Y-m-d', strtotime('now + 1day'));
+            $two_days_before_today = date('Y-m-d', strtotime('now - 2day'));
+            $this->logger->info('wizpay - L48 Search quotes from ' . $two_days_before_today . ', to ' . $tomorrow);
+            $quote_collection = $this->quoteCollection->addFieldToFilter('created_at', array('from' => $two_days_before_today, 'to' => $tomorrow))->loadWithFilter();
+            $this->logger->info('wizpay - L48 Reload all quotes');
             
-            $quotes = $this->quoteCollection->getItems();
+            $quotes =  $quote_collection->getItems();            
             
             if(is_array($quotes) && count($quotes) > 0){
+
+                $this->logger->info('wizpay - L60 get total quotes after get items ' .  count($quotes));
                 
                 foreach($quotes as $quote){
                     $paymentMethod = $quote->getPayment();
