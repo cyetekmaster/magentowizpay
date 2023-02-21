a39ed189a41a696074186dff46de48a8d0ebe939
liuyu0723 <liuyu0723@users.noreply.github.com>
2023-02-22 12:28:28 +1300

    turn off cron

--- Crons/ExecuteOrderCronJob.php
+++ Crons/ExecuteOrderCronJob.php
@@ -34,58 +34,58 @@ class ExecuteOrderCronJob {
         $this->logger->info('wizpay - ExecuteOrderCronJob Cron Works run every 5 mins');
         
         
-        $this->logger->info('wizpay - L33 Loading quotes'); 
-        $this->quoteCollection = $this->quoteCollectionFactory->create();
-        $this->logger->info('wizpay - L34 Loaded quotes');         
+        // $this->logger->info('wizpay - L33 Loading quotes'); 
+        // $this->quoteCollection = $this->quoteCollectionFactory->create();
+        // $this->logger->info('wizpay - L34 Loaded quotes');         
 
 
-        if($this->quoteCollection && $this->quoteCollection->getSize() > 0){
-            $this->logger->info('wizpay - L38 get total quotes before filter ' . $this->quoteCollection->getSize());
+        // if($this->quoteCollection && $this->quoteCollection->getSize() > 0){
+        //     $this->logger->info('wizpay - L38 get total quotes before filter ' . $this->quoteCollection->getSize());
 
-            $this->logger->info('wizpay - L45 Add filter for quote'); 
-            $tomorrow = date('Y-m-d', strtotime('now + 1day'));
-            $two_days_before_today = date('Y-m-d', strtotime('now - 2day'));
-            $this->logger->info('wizpay - L48 Search quotes from ' . $two_days_before_today . ', to ' . $tomorrow);
-            $quote_collection = $this->quoteCollection->addFieldToFilter('created_at', array('from' => $two_days_before_today, 'to' => $tomorrow))->loadWithFilter();
-            $this->logger->info('wizpay - L48 Reload all quotes');
+        //     $this->logger->info('wizpay - L45 Add filter for quote'); 
+        //     $tomorrow = date('Y-m-d', strtotime('now + 1day'));
+        //     $two_days_before_today = date('Y-m-d', strtotime('now - 2day'));
+        //     $this->logger->info('wizpay - L48 Search quotes from ' . $two_days_before_today . ', to ' . $tomorrow);
+        //     $quote_collection = $this->quoteCollection->addFieldToFilter('created_at', array('from' => $two_days_before_today, 'to' => $tomorrow))->loadWithFilter();
+        //     $this->logger->info('wizpay - L48 Reload all quotes');
             
-            $quotes =  $quote_collection->getItems();            
+        //     $quotes =  $quote_collection->getItems();            
             
-            if(is_array($quotes) && count($quotes) > 0){
+        //     if(is_array($quotes) && count($quotes) > 0){
 
-                $this->logger->info('wizpay - L60 get total quotes after get items ' .  count($quotes));
+        //         $this->logger->info('wizpay - L60 get total quotes after get items ' .  count($quotes));
                 
-                foreach($quotes as $quote){
-                    $paymentMethod = $quote->getPayment();
-                    if(isset($paymentMethod) && $paymentMethod->getMethod() == 'wizpay'){
+        //         foreach($quotes as $quote){
+        //             $paymentMethod = $quote->getPayment();
+        //             if(isset($paymentMethod) && $paymentMethod->getMethod() == 'wizpay'){
                         
-                        $additionalInformation = $paymentMethod->getAdditionalInformation();
+        //                 $additionalInformation = $paymentMethod->getAdditionalInformation();
                         
-                        if(isset($additionalInformation) && is_array($additionalInformation)
-                            && array_key_exists('token',$additionalInformation )
-                            && array_key_exists('transactionId',$additionalInformation )
-                            && array_key_exists('mer',$additionalInformation )){
+        //                 if(isset($additionalInformation) && is_array($additionalInformation)
+        //                     && array_key_exists('token',$additionalInformation )
+        //                     && array_key_exists('transactionId',$additionalInformation )
+        //                     && array_key_exists('mer',$additionalInformation )){
                                 
-                            $wz_token = $additionalInformation["token"];
-                            $wzTxnId = $additionalInformation["transactionId"];
-                            $merchantReference  = $additionalInformation["mer"];
+        //                     $wz_token = $additionalInformation["token"];
+        //                     $wzTxnId = $additionalInformation["transactionId"];
+        //                     $merchantReference  = $additionalInformation["mer"];
                             
-                            if(!empty($wz_token) && !empty($wzTxnId) && !empty($merchantReference)){
-                                $this->logger->info('wizpay - L64 process quote, QuoteId=' . $quote->getId() . ', mre=' . $merchantReference );
-                                try{
-                                    $this->wizpayOrderProcesser->process_quote($quote->getId(), $merchantReference);
-                                }catch (Exception $e) {
-                                    $this->logger->info('wizpay - L68 process quote error, QuoteId=' . $quote->getId() . ', mre=' . $merchantReference . ', error = '. $e->getMessage());
-                                }
-                            }
-                        }
-                    }
+        //                     if(!empty($wz_token) && !empty($wzTxnId) && !empty($merchantReference)){
+        //                         $this->logger->info('wizpay - L64 process quote, QuoteId=' . $quote->getId() . ', mre=' . $merchantReference );
+        //                         try{
+        //                             $this->wizpayOrderProcesser->process_quote($quote->getId(), $merchantReference);
+        //                         }catch (Exception $e) {
+        //                             $this->logger->info('wizpay - L68 process quote error, QuoteId=' . $quote->getId() . ', mre=' . $merchantReference . ', error = '. $e->getMessage());
+        //                         }
+        //                     }
+        //                 }
+        //             }
                     
     
-                }
-            }
+        //         }
+        //     }
             
             
-        }
+        // }
     }
 }
