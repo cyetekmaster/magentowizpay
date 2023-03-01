51c71c5648543a158feb698b5b3b6d2297695acf
liuyu0723 <liuyu0723@users.noreply.github.com>
2023-03-02 12:34:42 +1300

    fixed cart pricing issues

--- Helper/Data.php
+++ Helper/Data.php
@@ -860,7 +860,7 @@ class Data extends AbstractHelper
                     return $tValue .  '<div style="'. $this->wizpay_info_style_oneline . $this->wizpay_info_style_product_detail .'">
                         <img style="'. $this->wizpay_info_logo_style .'" src="' . $banktransferLogoUrl . '" /> 
                         <input type="hidden" id="wizpay-sub-amount-price-productid" name="wizpay-sub-amount-price-productid" value="' . $product_id . '">
-                        <span style="'. $this->wizpay_info_content_style .'">&nbsp;or 4 payments of <span id="wizpay-sub-amount-price">from <span id="wizpay-sub-amount-price">'. $sub_amount .'</span> to ' . $sub_amount2 .
+                        <span style="'. $this->wizpay_info_content_style .'">&nbsp;or 4 payments of <span id="wizpay-sub-amount-price">from '. $sub_amount1 . ' to ' . $sub_amount2 .
                         '</span> with Wizpay <a href="#" class="wizpay-learn-more-popup-link">learn more</a><span></div>';
                 }else{
                     // display full info
@@ -875,7 +875,8 @@ class Data extends AbstractHelper
             else if($type == 'Cart' && intval( $show_on_cat_page, 0) == 1){
                 return '<div style="'. $this->wizpay_info_style_oneline . $this->wizpay_info_style_product_detail .'">
                         <img style="'. $this->wizpay_info_logo_style .'" src="' . $banktransferLogoUrl . '" /> 
-                        <span style="'. $this->wizpay_info_content_style .'">&nbsp;or 4 payments of '. $sub_amount .' with Wizpay. 
+                        <span style="'. $this->wizpay_info_content_style .'">&nbsp;or 4 payments of <span id="wizpay-sub-amount-price">'. $sub_amount . 
+                        '</span> with Wizpay. 
                         <a href="#" class="wizpay-learn-more-popup-link">learn more</a><span></div>';
             }            
         }
